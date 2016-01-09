fe877dc0820ac0d1b80c95a8351daab5432daece
Alvin Cris Uy <alvincrisuy@gmail.com>
2016-01-10 07:10:47 +0800

    Add Camera ViewController

---
+++ PhotoDetection-iOS/PhotoDetection-iOS/Classes/Controller/PDCameraViewController/PDCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDCameraViewController.h
+//  PhotoDetection-iOS
+//
+//  Created by Cris Uy on 10/01/2016.
+//  Copyright © 2016 Alvin Cris Uy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDCameraViewController : UIViewController
+
+@end
