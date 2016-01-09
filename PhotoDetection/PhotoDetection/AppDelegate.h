0dea2e947d1a364559a95f1cb585ce3c5ef992cc
Alvin Cris Uy <alvincrisuy@gmail.com>
2016-01-09 23:30:15 +0800

    Initial project commit

---
+++ PhotoDetection/PhotoDetection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoDetection
+//
+//  Created by Cris Uy on 09/01/2016.
+//  Copyright © 2016 Alvin Cris Uy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
