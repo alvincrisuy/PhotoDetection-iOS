ae30e9f3cd876b48cfe49b1a149d68fdcceac5f7
Alvin Cris Uy <alvincrisuy@gmail.com>
2016-01-10 08:21:16 +0800

    Add transition/transfer in RootViewController

--- PhotoDetection-iOS/PhotoDetection-iOS/Classes/Controller/PDRootViewController/PDRootViewController.h
+++ PhotoDetection-iOS/PhotoDetection-iOS/Classes/Controller/PDRootViewController/PDRootViewController.h
@@ -8,6 +8,34 @@
 
 #import <UIKit/UIKit.h>
 
+@class PDCameraViewController;
+
 @interface PDRootViewController : UIViewController
+
+// Rootview UIViews
+@property (nonatomic, retain) IBOutlet UIView *navigationView;
+@property (nonatomic, retain) IBOutlet UIView *contentView;
+
+// Navigation Controller - Navigate Pages
+@property (nonatomic, retain) UINavigationController *contentNavigationController;
+
+// Back Button
+@property (nonatomic, retain) IBOutlet UIView *backButtonView;
+@property (nonatomic, retain) IBOutlet UIButton *backButton;
+
+// Transitions
+- (void)transitionCamera;
+
+- (void)transition:(UIViewController *)viewController animated:(BOOL)animated;
+- (void)popTransitionAnimated:(BOOL)animated;
+- (void)pushTransition:(UIViewController *)viewController animated:(BOOL)animated;
+
+// Transfer
+- (void)transferCameraViewController:(id)sender;
+
+- (PDCameraViewController *)cameraViewController;
+
+// Singleton
++ (PDRootViewController *)sharedController;
 
 @end
--- PhotoDetection-iOS/PhotoDetection-iOS/Classes/Controller/PDRootViewController/PDRootViewController.m
+++ PhotoDetection-iOS/PhotoDetection-iOS/Classes/Controller/PDRootViewController/PDRootViewController.m
@@ -7,16 +7,52 @@
 //
 
 #import "PDRootViewController.h"
+#import "PDCameraViewController.h"
 
-@interface PDRootViewController ()
+static PDRootViewController *_sharedController = nil;
+
+@interface PDRootViewController () {
+    PDCameraViewController *_cameraViewController;
+}
+
+// Transition
+- (void)hiddenAllPage;
+
+// Back Button
+- (IBAction)backButtonAction:(id)sender;
 
 @end
 
 @implementation PDRootViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+    }
+    
+    _sharedController = self;
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    NSLog(@"Size: %f %f %f %f",
+          self.view.frame.size.width,
+          self.view.frame.size.height,
+          self.view.frame.origin.x,
+          self.view.frame.origin.y);
+    
+    NSLog(@"Bounds: %f %f %f %f",
+          self.view.bounds.size.width,
+          self.view.bounds.size.height,
+          self.view.bounds.origin.x,
+          self.view.bounds.origin.y);
+    
+    [self transitionCamera];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +60,120 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Back Button
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)backButtonAction:(id)sender {
+    [self popTransitionAnimated:YES];
 }
-*/
+
+#pragma mark - Transitions
+
+- (void)transitionCamera {
+    [self hiddenAllPage];
+    
+    [self transition:self.cameraViewController animated:NO];
+}
+
+
+- (void)hiddenAllPage {
+    if (self.contentNavigationController) {
+        [self.contentNavigationController popToRootViewControllerAnimated:NO];
+    }
+    
+    NSArray* views = [self.contentView subviews];
+    for (UIView* view in views) {
+        [view removeFromSuperview];
+    }
+}
+
+- (void)transition:(UIViewController *)viewController animated:(BOOL)animated {
+    if (self.contentNavigationController != nil) {
+        self.contentNavigationController = nil;
+    }
+    
+    self.contentNavigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    
+    [self.contentNavigationController.view setFrame:self.contentView.bounds];
+    
+    self.backButtonView.alpha = 0;
+    
+    viewController.view.alpha = 1;
+    
+    CATransition* animation;
+    animation      = [CATransition animation];
+    animation.type = kCATransitionFade;
+    {
+        [self.contentView addSubview:self.contentNavigationController.view];
+    }
+    
+    if (animated) {
+        [self.contentView.layer addAnimation:animation forKey:nil];
+    }
+    
+    [self.contentNavigationController setNavigationBarHidden:YES animated:NO];
+    [self.contentNavigationController viewDidAppear:YES];
+}
+
+- (void)pushTransition:(UIViewController *)viewController animated:(BOOL)animated {
+    CATransition* animation;
+    animation      = [CATransition animation];
+    animation.type = kCATransitionFade;
+    {
+        self.backButtonView.alpha = 1;
+    }
+    
+    if (animated) {
+        [self.backButtonView.layer addAnimation:animation forKey:nil];
+    }
+    
+    viewController.view.frame = self.contentView.bounds;
+    
+    [self.contentNavigationController pushViewController:viewController animated:animated];
+}
+
+- (void)popTransitionAnimated:(BOOL)animated {
+    [self.contentNavigationController popViewControllerAnimated:animated];
+    
+    if ([[self.contentNavigationController viewControllers] count] == 1) {
+        CATransition* animation;
+        animation      = [CATransition animation];
+        animation.type = kCATransitionFade;
+        {
+            self.backButtonView.alpha = 0;
+        }
+        
+        if (animated) {
+            [self.backButtonView.layer addAnimation:animation forKey:nil];
+        }
+    }
+}
+
+#pragma mark - Transfer
+
+- (void)transferCameraViewController:(id)sender {
+    [self pushTransition:self.cameraViewController animated:YES];
+}
+
+- (PDCameraViewController *)cameraViewController {
+    if (!_cameraViewController) {
+        _cameraViewController = [[PDCameraViewController alloc] init];
+        _cameraViewController.view.frame = self.contentView.bounds;
+    }
+    
+    return _cameraViewController;
+}
+
+#pragma mark - Singleton
+
++ (PDRootViewController *)sharedController {
+    
+    @synchronized(self) {
+        if (_sharedController == nil) {
+            _sharedController = [[self alloc] init];
+        }
+    }
+    
+    return _sharedController;
+}
 
 @end
